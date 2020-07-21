c38ec39aaf88b08b4493f0f88123830605e50770
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2020-07-21 14:28:43 +0200

    project set up rendering custom component and showing server side code match error

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <HelloA />
+  </div>
+</template>
+
+<script>
+import { HelloA } from 'vue-package-test-x';
+
+export default {
+  components: {
+    HelloA,
+  },
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
